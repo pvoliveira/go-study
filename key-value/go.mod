9847085b8a722c5ad28cbe85c860c4e0e294b57d
Paulo Oliveira <paulovt7@gmail.com>
2022-10-15 14:00:36 +0100

    feat: use gorilla/mux and routes with vars

---
+++ key-value/go.mod
@@ -0,0 +1,5 @@
+module github.com/pvoliveira/go-study/key-value
+
+go 1.19
+
+require github.com/gorilla/mux v1.8.0
--- key-value/main.go
+++ key-value/main.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello gorilla/mux!\n"))
+}
+
+func productsHandler(w http.ResponseWriter, r *http.Request) {
+	logVars(r)
+}
+
+func articlesCategoryHandler(w http.ResponseWriter, r *http.Request) {
+	logVars(r)
+}
+
+func articleHandler(w http.ResponseWriter, r *http.Request) {
+	logVars(r)
+}
+
+func logVars(r *http.Request) {
+	vars := mux.Vars(r)
+	for k, v := range vars {
+		log.Printf("%v: %v", k, v)
+	}
+}
+
 func main() {
-	panic("not implemented")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", handler)
+
+	r.HandleFunc("/products/{key}", productsHandler)
+	r.HandleFunc("/articles/{category}/", articlesCategoryHandler)
+	r.HandleFunc("/articles/{category}/{id:[0-9]+}", articleHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
